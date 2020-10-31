7e8976f9e7d083bd3fae26e0d1e61828118117eb
Thomas Allard <thomasallard@free.fr>
2020-10-31 02:22:38 +0100

    Initial setup

---
+++ src/main/kotlin/com/tallard/mhgu/MhguApplication.kt
@@ -0,0 +1,11 @@
+package com.tallard.mhgu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MhguApplication
+
+fun main(args: Array<String>) {
+	runApplication<MhguApplication>(*args)
+}
---
+++ src/test/kotlin/com/tallard/mhgu/MhguApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tallard.mhgu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MhguApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
